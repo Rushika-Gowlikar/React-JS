c3f07f8ff700ccaaaa2468ed5944e47a2a3c56a3
Rushika-Gowlikar <rushikagowlikar@gmail.com>
2024-08-02 18:46:26 +0530

    updated hook changes

--- src/components/learn/learn.js
+++ src/components/learn/learn.js
@@ -1,7 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useReducer, useState } from 'react'
 import './learn.css'
+
 export default function Learn(){
- const [initialValue, setNewValue]= useState("Neo")
+ 
+ 
+    
+const [initialValue, setNewValue]= useState("Neo");
+const [state,dispatch]= useReducer(reducer,{age:1});
+
+useEffect(()=>{
+ console.log("update data")
+})
+function reducer(state,action ){
+if(action.type==="increment_age"){
+    return {age: state.age+1 }
+}
+}
+function handleClick(){
+dispatch ({type: 'increment_age' })
+}
   function UpdateValue($event){
 setNewValue($event.target.value)
   }
@@ -10,5 +27,8 @@ setNewValue($event.target.value)
     return (<div className='learn-page'>
         <input type='text' className='inputField' value={initialValue} onChange={UpdateValue}/><br/>
         {initialValue}
+        <br/>
+        <button className="btn" onClick={handleClick}>Increment value</button>
+        <p>Hello! You are {state.age}.</p>
     </div>)
 }
